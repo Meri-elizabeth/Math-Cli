def4ab0b58011f2eaae84ea9ba1f36e02d0c786d
Merilyn <merilynmawlieh14@gmail.com>
2022-04-08 13:57:18 +0530

    Created math.py and a README.md file

---
+++ README.md
@@ -0,0 +1,13 @@
+# Math CLI
+## Usage
+```
+math add 2 3
+5
+
+math sub 2 3
+-1
+
+math mul 2 3
+6
+
+```
---
+++ math.py
@@ -0,0 +1,13 @@
+from audioop import add
+import sys
+
+operation = sys.argv[1]
+
+num1=int(sys.argv[2])
+num2=int(sys.argv[3])
+print(operation)
+print (num1+num2)
+
+if 
+
+
