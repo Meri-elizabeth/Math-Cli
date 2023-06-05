72235211c6d8478a823da754239ec1e2f8a099a1
Merilyn <merilynmawlieh14@gmail.com>
2023-06-05 22:30:25 +0530

    Complete basic commands

--- math.py
+++ math.py
@@ -1,13 +1,12 @@
-from audioop import add
 import sys
-
 operation = sys.argv[1]
-
-num1=int(sys.argv[2])
-num2=int(sys.argv[3])
-print(operation)
-print (num1+num2)
-
-if 
-
-
+num1 = int(sys.argv[2])
+num2 = int(sys.argv[3])
+if operation == "add":
+    print(num1 + num2)
+elif operation == "mul":
+    print(num1 * num2)
+elif operation == "sub":
+    print(num1 - num2)
+else:
+    print("Invalid operation. Please use 'add', 'mul', or 'sub'.")
