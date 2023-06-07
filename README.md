9f92c59c860e035c7b8b95bd79321bcd6d51f7e1
Merilyn <merilynmawlieh14@gmail.com>
2023-06-07 21:42:39 +0530

    Added combine operation

--- README.md
+++ README.md
@@ -10,4 +10,11 @@ math sub 2 3
 math mul 2 3
 6
 
+To run the program simply enter :
+python3 mathcli.py add num1 num2
+python3 mathcli.py mul num1 num2
+python3 mathcli.py sub num1 num2
+python3 mathcli.py divide num1 num2
+python3 mathcli.py combine num1 num2 mul num3
+
 ```
--- math.py
+++ math.py
@@ -2,11 +2,28 @@ import sys
 operation = sys.argv[1]
 num1 = int(sys.argv[2])
 num2 = int(sys.argv[3])
+
 if operation == "add":
     print(num1 + num2)
 elif operation == "mul":
     print(num1 * num2)
 elif operation == "sub":
     print(num1 - num2)
+elif operation == "combine":
+    result = num1 + num2
+    operation2 = sys.argv[4]
+    num3 = int(sys.argv[5])
+
+    if operation2 == "mul":
+        result *= num3
+    elif operation2 == "sub":
+        result -= num3
+    elif operation2 == "add":
+        result += num3
+    elif operation2 == "divide":
+        result /= num3
+    else:
+        print("Invalid additional operation. Please use 'mul' or 'sub'.")
+    print(result)
 else:
     print("Invalid operation. Please use 'add', 'mul', or 'sub'.")
